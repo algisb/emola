2a69ea5b883bfa019eb3c0a387acf8b09a04de78
algisb <bob@lavono.lavono>
2019-11-24 16:35:07 +0000

    Halt instruction

--- src/cpu.c
+++ src/cpu.c
@@ -44,10 +44,10 @@ typedef struct Regs Regs;
 #define SHOW_DEBUG_INFO
 
 #ifdef SHOW_DEBUG_INFO
-#define PRINT_INSTRUCTION(_str) \
+#define PRINT_DEBUG(_str) \
 printf(_str)
 #else
-#define PRINT_INSTRUCTION(_str)
+#define PRINT_DEBUG(_str)
 #endif
 
 
@@ -104,21 +104,116 @@ int feDeExInst(CPU * _cpu, unsigned char * _memory)
     }
     else
     {
-        Opcode opcode = decodeOp(0b11000000/*_cpu->regs.IR*/);
+        Opcode opcode = decodeOp(_cpu->regs.IR);
         
         
-        printf("data: %d\n", opcode.data);
-        printf("x: %d\n", opcode.x);
-        printf("y: %d\n", opcode.y);
-        printf("z: %d\n", opcode.z);
-        printf("p: %d\n", opcode.p);
-        printf("q: %d\n", opcode.q);
+//         printf("data: %d\n", opcode.data);
+//         printf("x: %d\n", opcode.x);
+//         printf("y: %d\n", opcode.y);
+//         printf("z: %d\n", opcode.z);
+//         printf("p: %d\n", opcode.p);
+//         printf("q: %d\n", opcode.q);
+        
+        switch(opcode.x)
+        {
+            case 0:
+            {
+                switch(opcode.z)
+                {
+                    case 0:
+                    {
+                        switch(opcode.y)
+                        {
+                            case 0:
+                            {
+                                PRINT_DEBUG("NOP\n");
+                                _cpu->cycles += 4;
+                                _cpu->regs.PC += 1;
+                                break;
+                            }
+                            case 1:
+                            {
+                                PRINT_DEBUG("LD (nn) SP\n");
+                                unsigned short * loc = (unsigned short *)(&_memory[regs->PC + 1]);
+                                *loc = _cpu->regs.SP;
+                                _cpu->cycles += 20;
+                                _cpu->regs.PC += 3;
+                                break;
+                            }
+                            default:
+                            {
+                                printf("Error: unhandled opcode x: %d z: %d (y: %d)\n", opcode.x, opcode.z, opcode.y);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    
+                    default:
+                    {
+                        printf("Error: unhandled opcode x: %d (z: %d)  y: %d)\n", opcode.x, opcode.z, opcode.y);
+                        break;
+                    }
+                }
+                break;
+            }
+            
+            
+            case 1:
+            {
+                switch(opcode.z)
+                {
+                    case 6:
+                    {
+                        switch(opcode.y)
+                        {
+                            case 6:
+                            {
+                                PRINT_DEBUG("HALT\n");
+                                _cpu->cycles += 4;
+                                _cpu->regs.PC += 1;
+                                while(1);
+                                break;
+                            }
+                            default:
+                            {
+                                printf("Error: unhandled opcode x: %d z: %d (y: %d)\n", opcode.x, opcode.z, opcode.y);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    default:
+                    {
+                        printf("Error: unhandled opcode x: %d (z: %d)  y: %d)\n", opcode.x, opcode.z, opcode.y);
+                        break;
+                    }
+                }
+                break;
+            }
+            
+            
+            case 2:
+            {
+                break;
+            }
+            case 3:
+            {
+                break;
+            }
+            
+            default:
+            {
+                printf("Error: unhandled opcode (x: %d) z: %d y: %d)\n", opcode.x, opcode.z, opcode.y);
+                break;
+            }
+        }
         
 
             //8 bit loads---------------------------------------------------------------------------------
 //             case 0x3E:
 //             {
-//                 PRINT_INSTRUCTION("LD A *\n");
+//                 PRINT_DEBUG("LD A *\n");
 //                 regs->A = _memory[regs->PC + 1];//access the immediate memory going after the instruction
 //                 _cpu->regs.PC += 2;
 //                 _cpu->cycles += 8;
@@ -129,7 +224,7 @@ int feDeExInst(CPU * _cpu, unsigned char * _memory)
             //16 bit loads---------------------------------------------------------------------------------
 //             case 0x01:
 //             {
-//                 PRINT_INSTRUCTION("LD BC **\n");
+//                 PRINT_DEBUG("LD BC **\n");
 //                 _cpu->regs.L = _memory[regs->PC + 1];
 //                 _cpu->regs.D = _memory[regs->PC + 2];
 //                 
@@ -138,7 +233,7 @@ int feDeExInst(CPU * _cpu, unsigned char * _memory)
 //                 break;
 //             }
         
-            while(1);//remove to continue execution
+//            while(1);//remove to continue execution
                 //printf("Uninplemented instruction: 0x%02x\n", _cpu->regs.IR);
                 
     }
